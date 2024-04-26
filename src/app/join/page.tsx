3cde3f8885c84d7f0177856ade188d27c03827a7
Chris Lansing <cslansin@gmail.com>
2024-04-26 10:24:35 -0400

    Remove unused route

--- src/app/join/page.tsx
+++
@@ -1,9 +0,0 @@
-import JoinRoom from "@/components/forms/JoinRoom";
-
-export default function Join() {
-  return (
-    <div className="w-full h-full flex justify-center items-center">
-      <JoinRoom />
-    </div>
-  );
-}
