7a1030cf6378e7f7efdaa879e22199dd93ea9282
Chris Lansing <cslansin@gmail.com>
2024-04-26 12:15:19 -0400

    Remove alpha layer from canvas. Bisect here if fps stinks

--- src/game/Scenes/Casino.tsx
+++ src/game/Scenes/Casino.tsx
@@ -8,7 +8,6 @@ import { Stats } from "@react-three/drei";
 import "@/game/Loaders/AssetLoader";
 import { authAtom } from "@/state/atoms";
 import LoadingProgress from "@/game/Loaders/LoadingProgress";
-import BaseLighting from "@/game/Staging/BaseLighting";
 import TableCamera from "@/game/Cameras/TableCamera";
 import TableControls from "@/game/Controls/TableControls";
 import TableView from "@/game/Views/TableView";
@@ -21,15 +20,15 @@ export default function CardShow() {
       <Canvas
         frameloop="demand"
         dpr={[1, 2]}
-        gl={{ antialias: true, alpha: true }}
+        gl={{ antialias: true, alpha: false }}
         flat
       >
+        <color attach="background" args={["#daf5f0"]} />
         <TableCamera />
         <TableControls />
         <Suspense fallback={<LoadingProgress />}>
           <TableView />
         </Suspense>
-        <BaseLighting />
         <Stats />
       </Canvas>
     </div>
