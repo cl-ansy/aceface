adf86ecaf3f5adf46bd10a8e9f13fd5d58a69b8f
Chris Lansing <cslansin@gmail.com>
2024-04-30 22:05:29 -0400

    Remove login page

--- src/app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import { LoginForm } from "@/components/forms/LoginForm";
-
-export default function Login() {
-  return (
-    <div className="flex justify-center">
-      <LoginForm />
-    </div>
-  );
-}
