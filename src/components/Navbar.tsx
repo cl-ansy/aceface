2c5474ce1eb51016f46da22eb5421bf2772f92a2
Chris Lansing <cslansin@gmail.com>
2024-05-01 09:49:03 -0400

    Fix pointer events for navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,7 +14,7 @@ function Navbar() {
   const balance = useAtomValue(balanceAtom);
 
   return (
-    <nav className="fixed left-0 top-0 z-10 mx-auto flex h-20 w-full items-center px-5 m500:h-16 ">
+    <nav className="pointer-events-none fixed left-0 top-0 z-10 mx-auto flex h-20 w-full items-center px-5 m500:h-16">
       <div className="mx-auto flex w-[1252px] max-w-full px-12 items-center justify-between">
         <div className="flex items-center gap-10 m400:flex-1 m400:pl-5">
           <Link className="text-4xl font-bold m500:text-xl" href={"/"}>
@@ -24,7 +24,7 @@ function Navbar() {
 
         <div className="flex items-center gap-10 m700:hidden"></div>
 
-        <div className="flex items-center justify-end gap-2">
+        <div className="pointer-events-auto flex items-center justify-end gap-2">
           {userUid && balance && (
             <Badge className="font-bold" text={String(balance)} />
           )}
