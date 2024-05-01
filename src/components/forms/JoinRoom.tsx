85131ed1ea53753a83c72d48a75eece007bf77b2
Chris Lansing <cslansin@gmail.com>
2024-04-30 22:18:34 -0400

    Fix case sensitive imports with 'git config core.ignorecase false'

---
+++ src/components/forms/JoinRoom.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useState } from "react";
+
+export default function JoinRoom() {
+  const [roomId, setRoomId] = useState("");
+
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.nativeEvent.preventDefault();
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex w-min items-center rounded-base border-2 border-black font-base shadow-base"
+      role="form"
+    >
+      <input
+        className="w-[30ch] rounded-base p-[10px] outline-none"
+        type="text"
+        name="roomId"
+        id="roomId"
+        placeholder="Enter Room ID"
+        value={roomId}
+        onChange={(e) => {
+          setRoomId(e.target.value);
+        }}
+      />
+      <button
+        className="rounded-e-[5px] border-l-2 border-black bg-main p-[10px] px-5"
+        type="submit"
+        aria-label="Join"
+      >
+        Join
+      </button>
+    </form>
+  );
+}
---
+++ src/components/forms/RegisterForm.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { useState } from "react";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Button } from "@/components/ui/Button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/Form";
+import { Label } from "@/components/ui/Label";
+import { Switch } from "@/components/ui/Switch";
+import { Input } from "@/components/ui/Input";
+
+const formSchema = z.object({
+  email: z.string().email("Invalid email"),
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters")
+    .max(128, "Password cannot be more than 128 characters"),
+});
+
+export function RegisterForm() {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="sr-only">Email</FormLabel>
+              <FormControl>
+                <Input type="email" placeholder="Email" {...field} />
+              </FormControl>
+              {/* <FormDescription>Email</FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="sr-only">Password</FormLabel>
+              <FormControl>
+                <Input
+                  type={passwordVisible ? "text" : "password"}
+                  placeholder="Password"
+                  {...field}
+                />
+              </FormControl>
+              {/* <FormDescription>Password</FormDescription> */}
+              <FormMessage />
+              <div className="flex items-center py-4 space-x-2">
+                <Switch
+                  tabIndex={-1}
+                  id="show-password"
+                  checked={passwordVisible}
+                  onCheckedChange={setPasswordVisible}
+                />
+                <Label htmlFor="show-password"> Show Password</Label>
+              </div>
+            </FormItem>
+          )}
+        />
+        <Button className="w-full" type="submit">
+          Create account
+        </Button>
+      </form>
+    </Form>
+  );
+}
