af43d47768db1d67db21398130608fa0e482cae9
Chris Lansing <cslansin@gmail.com>
2024-05-02 09:23:26 -0400

    Move Progress into separate ui component

--- src/game/Loaders/LoadingProgress.tsx
+++ src/game/Loaders/LoadingProgress.tsx
@@ -1,28 +1,6 @@
-import { forwardRef } from "react";
 import { Html, useProgress } from "@react-three/drei";
-import * as ProgressPrimitive from "@radix-ui/react-progress";
 
-import { cn } from "@/lib/utils";
-
-const Progress = forwardRef<
-  React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...props }, ref) => (
-  <ProgressPrimitive.Root
-    ref={ref}
-    className={cn(
-      "relative h-4 w-full overflow-hidden rounded-full border-2 border-black bg-slate-100",
-      className
-    )}
-    {...props}
-  >
-    <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 border-r-2 border-black bg-main transition-all"
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
-    />
-  </ProgressPrimitive.Root>
-));
-Progress.displayName = ProgressPrimitive.Root.displayName;
+import { Progress } from "@/components/ui/Progress";
 
 export default function LoadingProgress() {
   const { progress } = useProgress();
