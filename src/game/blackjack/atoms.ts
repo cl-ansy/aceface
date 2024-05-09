691332b9d9f577e4f230b25d7521bcc8179d217e
Chris Lansing <cslansin@gmail.com>
2024-05-09 16:18:09 -0400

    Adding atoms and types for blackjack game state

---
+++ src/game/blackjack/atoms.ts
@@ -0,0 +1,61 @@
+import { atom } from "jotai";
+import { atomFamily } from "jotai/utils";
+
+import type { Game, Players } from "@/game/blackjack/blackjackTypes";
+
+const initialGameData = {
+  type: "blackjack",
+  rules: {
+    deckCount: 6,
+    penetration: 0.65,
+  },
+  shoe: {
+    shoeUid: "",
+    remaining: 20,
+  },
+  round: {
+    roundUid: "",
+    status: "new",
+    turn: 1,
+  },
+  players: {
+    1: null,
+    2: null,
+    3: {
+      handUid: "",
+      userUid: "asdf",
+      displayName: "Wowza",
+      hand: {
+        0: "AH",
+        1: "QH",
+      },
+    },
+    4: null,
+    5: null,
+  },
+  dealer: {
+    handUid: "",
+    hand: {
+      0: "0",
+      1: "KS",
+    },
+  },
+};
+
+const gameAtom = atom<Game>(initialGameData);
+
+export const gameUidAtom = atom("");
+
+export const rulesAtom = atom((get) => get(gameAtom).rules);
+
+export const shoeAtom = atom((get) => get(gameAtom).shoe);
+
+export const roundAtom = atom((get) => get(gameAtom).round);
+
+export const playersAtom = atom((get) => get(gameAtom).players);
+
+export const playerFamily = atomFamily((playerNum) =>
+  atom((get) => get(playersAtom)[playerNum as keyof Players]),
+);
+
+export const dealerAtom = atom((get) => get(gameAtom).dealer);
---
+++ src/game/blackjack/blackjackTypes.ts
@@ -0,0 +1,48 @@
+export type Rules = {
+  deckCount: number;
+  penetration: number;
+};
+
+export type Shoe = {
+  shoeUid: string;
+  remaining: number;
+};
+
+export type Round = {
+  roundUid: string;
+  status: string;
+  turn: number;
+};
+
+export type Hand = {
+  [key: number]: string;
+};
+
+export type Player = {
+  handUid: string;
+  userUid: string;
+  displayName: string;
+  hand: Hand;
+};
+
+export type Players = {
+  1: Player | null;
+  2: Player | null;
+  3: Player | null;
+  4: Player | null;
+  5: Player | null;
+};
+
+export type Dealer = {
+  handUid: string;
+  hand: Hand;
+};
+
+export type Game = {
+  type: string;
+  rules: Rules;
+  shoe: Shoe;
+  round: Round;
+  players: Players;
+  dealer: Dealer;
+};
